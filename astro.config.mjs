6eef17177c507d516c3486072fddc414177adfb3
Daniel Rodriguez <daniel@danielfrg.com>
2022-05-06 14:52:31 -0500

    Astro init

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+import { defineConfig } from 'astro/config';
+
+import svelte from "@astrojs/svelte";
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [svelte(), tailwind()]
+});
