c63e38c4d37bfe402dfdbd3a602ce12f2c593c7b
Daniel Rodriguez <daniel@danielfrg.com>
2022-05-07 13:34:04 -0500

    Secrets

--- src/firebase.js
+++ src/firebase.js
@@ -3,51 +3,51 @@ let resolve, firebaseInstance, firestoreInstance;
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.PUBLIC_FIREBASE_API_KEY,
-  authDomain: "demucs-service.firebaseapp.com",
-  projectId: "demucs-service",
-  storageBucket: "demucs-service.appspot.com",
-  appId: 1:259910459855:web:35a784beb610343b5732ba,
-  // authDomain: import.meta.env.PUBLIC_AUTH_DOMAIN,
-  // projectId: import.meta.env.PUBLIC_PROJECT_ID,
-  // storageBucket: import.meta.env.PUBLIC_STORAGE_BUCKET,
-  // appId: import.meta.env.PUBLIC_APP_ID,
+    apiKey: import.meta.env.PUBLIC_FIREBASE_API_KEY,
+    authDomain: "demucs-service.firebaseapp.com",
+    projectId: "demucs-service",
+    storageBucket: "demucs-service.appspot.com",
+    appId: "1:259910459855:web:35a784beb610343b5732ba",
+    // authDomain: import.meta.env.PUBLIC_AUTH_DOMAIN,
+    // projectId: import.meta.env.PUBLIC_PROJECT_ID,
+    // storageBucket: import.meta.env.PUBLIC_STORAGE_BUCKET,
+    // appId: import.meta.env.PUBLIC_APP_ID,
 };
 
 const promise = new Promise((res) => (resolve = res));
 
 export async function initialize() {
-  if (import.meta.env.SSR) return undefined;
-  if (firebaseInstance) return firebaseInstance;
+    if (import.meta.env.SSR) return undefined;
+    if (firebaseInstance) return firebaseInstance;
 
-  firebaseInstance = initializeApp(firebaseConfig);
-  resolve(firebaseInstance);
-  return firebaseInstance;
+    firebaseInstance = initializeApp(firebaseConfig);
+    resolve(firebaseInstance);
+    return firebaseInstance;
 }
 
 export async function getInstance() {
-  // if (import.meta.env.SSR) return undefined;
-  // if (firebaseInstance) return firebaseInstance;
-  // return promise;
-  if (import.meta.env.SSR) return undefined;
-  if (firebaseInstance) return firebaseInstance;
+    // if (import.meta.env.SSR) return undefined;
+    // if (firebaseInstance) return firebaseInstance;
+    // return promise;
+    if (import.meta.env.SSR) return undefined;
+    if (firebaseInstance) return firebaseInstance;
 
-  firebaseInstance = initializeApp(firebaseConfig);
-  resolve(firebaseInstance);
-  return firebaseInstance;
+    firebaseInstance = initializeApp(firebaseConfig);
+    resolve(firebaseInstance);
+    return firebaseInstance;
 }
 
 export async function getFirestore() {
-  if (firestoreInstance) return firestoreInstance;
+    if (firestoreInstance) return firestoreInstance;
 
-  const { getFirestore } = import("firebase/firestore");
-  await getInstance();
-  firestoreInstance = getFirestore();
-  return firestoreInstance;
+    const { getFirestore } = import("firebase/firestore");
+    await getInstance();
+    firestoreInstance = getFirestore();
+    return firestoreInstance;
 }
 
 export default {
-  initialize,
-  getInstance,
-  getFirestore,
+    initialize,
+    getInstance,
+    getFirestore,
 };
